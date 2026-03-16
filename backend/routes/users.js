bed40d877f08d1c9c5f66e5262f9d52e2c53a599
Anjana Baskaran <anjanabaskaran2005@gmail.com>
2026-03-16 23:45:30 +0530

    Fix user routes and remove seed.js to deploy changes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,6 +6,8 @@ const {
   getTeamMembers,
   getManagers,
   addUser,
+  updateUser,
+  assignTeam,
   removeUser,
 } = require('../controllers/userController');
 const { auth, authorize } = require('../middleware/auth');
@@ -16,8 +18,10 @@ router.get('/profile', auth, getProfile);
 router.put('/profile', auth, updateProfile);
 router.get('/team', auth, getTeamMembers);
 router.get('/managers', auth, getManagers);
+router.post('/assign-team', auth, authorize('director'), assignTeam);
 router.get('/', auth, authorize('director'), getAllUsers);
 router.post('/', auth, authorize('director'), addUser);
+router.put('/:id', auth, authorize('director'), updateUser);
 router.delete('/:id', auth, authorize('director'), removeUser);
 
 module.exports = router;
